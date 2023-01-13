dfc34c5a36dd98a93ca5d243d5fb71ce76456b6e
sinyoung yun <yeogksquf@gmail.com>
2023-01-13 10:42:18 +0900

    start project
    - setting build.gradle

---
+++ src/main/java/com/ysy/dodamdodam/DodamdodamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ysy.dodamdodam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodamdodamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodamdodamBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ysy/dodamdodam/DodamdodamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysy.dodamdodam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodamdodamBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
